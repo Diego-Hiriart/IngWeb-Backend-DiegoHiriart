ec803e81983570d9c3260d1c6f86a5b1f8eb6a45
Diego Hiriart <hiriart.leon.d@gmail.com>
2022-05-10 02:36:00 -0500

    Update APIConfig.cs

--- APIConfig.cs
+++ APIConfig.cs
@@ -16,12 +16,9 @@ namespace WebAPI_DiegoHiriart
 
         private static string token = "A_super secret key-for the T0kenS";//A key to create the login tokens
 
-        private static UserDto admin = new UserDto(1, "diego.hiriart@udla.edu.ec", "SysAdmin", "");
-
         public static string ConnectionString { get => connectionString; set => connectionString = value; }
 
         public static string Token { get => token; }
 
-        public static UserDto Admin { get => admin; }
     }
 }
